7e6e2378f197b1866948c927a086fefde50bea8c
Volki-v-boi <152771192+Volki-v-boi@users.noreply.github.com>
2024-05-03 10:52:30 +0200

    hw-10-done-unstyled

---
+++ src/js/1-timer.js
@@ -0,0 +1,102 @@
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+let userSelectedDate;
+
+flatpickr('#datetime-picker', {
+  onClose: function (selectedDates) {
+    userSelectedDate = selectedDates[0];
+    const currentDate = new Date();
+    if (userSelectedDate < currentDate) {
+      iziToast.error({ title: 'Please choose a date in the future' });
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+});
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+startButton.addEventListener('click', function () {
+  const countdownInterval = setInterval(updateCountdown, 1000);
+
+  startButton.disabled = true;
+  datetimePicker.disabled = true;
+
+  function updateCountdown() {
+    const currentDate = new Date();
+    const difference = userSelectedDate - currentDate;
+
+    if (difference <= 0) {
+      clearInterval(countdownInterval);
+      daysValue.textContent = '00';
+      hoursValue.textContent = '00';
+      minutesValue.textContent = '00';
+      secondsValue.textContent = '00';
+
+      datetimePicker.disabled = false;
+      startButton.disabled = false;
+
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(difference);
+
+    daysValue.textContent = addLeadingZero(days);
+    hoursValue.textContent = addLeadingZero(hours);
+    minutesValue.textContent = addLeadingZero(minutes);
+    secondsValue.textContent = addLeadingZero(seconds);
+  }
+
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  }
+
+  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+});
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,31 @@
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = parseInt(document.querySelector('input[name="delay"]').value);
+  const status = document.querySelector('input[name="state"]:checked').value;
+
+  const promise = new Promise((resolve, reject) => {
+    if (status === 'fulfilled') {
+      setTimeout(() => {
+        resolve(delay);
+      }, delay);
+    } else {
+      setTimeout(() => {
+        reject(delay);
+      }, delay);
+    }
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({ title: `✅ Fulfilled promise in ${delay}ms` });
+    })
+    .catch(delay => {
+      iziToast.error({ title: `❌ Rejected promise in ${delay}ms` });
+    });
+});
+
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
